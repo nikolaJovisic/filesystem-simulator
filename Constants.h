33a507328f8caf9d2d913d57aa4cbd3048cbe4f1
ra74-2018 <nikolajovisic4@gmail.com>
2022-02-13 23:43:23 +0100

    chore: init constants file

---
+++ Constants.h
@@ -0,0 +1,10 @@
+//
+// Created by paracelsus on 2/13/2022.
+//
+
+#ifndef FILESYSTEM_SIMULATOR_CONSTANTS_H
+#define FILESYSTEM_SIMULATOR_CONSTANTS_H
+
+const float DESCRIPTORS_MEMORY_SHARE = 0.1; //share of persistent memory capacity dedicated to descriptors
+
+#endif //FILESYSTEM_SIMULATOR_CONSTANTS_H
