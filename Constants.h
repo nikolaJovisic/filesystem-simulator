c4eb0b930df82da5f3e4873e5d766cdc7f3d5baf
ra74-2018 <nikolajovisic4@gmail.com>
2022-02-14 16:35:45 +0100

    chore: update constants

--- Constants.h
+++ Constants.h
@@ -5,6 +5,10 @@
 #ifndef FILESYSTEM_SIMULATOR_CONSTANTS_H
 #define FILESYSTEM_SIMULATOR_CONSTANTS_H
 
+#include <string>
+
 const float DESCRIPTORS_MEMORY_SHARE = 0.1; //share of persistent memory capacity dedicated to descriptors
+const unsigned NUMBER_OF_BLOCKS_RESERVED_FOR_ROOT_DIRECTORY = 1;
+const char DELIMITER = '/';
 
 #endif //FILESYSTEM_SIMULATOR_CONSTANTS_H
