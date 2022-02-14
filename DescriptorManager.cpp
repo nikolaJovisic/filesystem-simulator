aa624ef9319a20974f72dfe5475b8b3a08309eaa
ra74-2018 <nikolajovisic4@gmail.com>
2022-02-14 16:36:26 +0100

    refactor: rename descriptor manager, add updateDescriptor

--- DescriptorManager.cpp
+++ DescriptorManager.cpp
@@ -2,49 +2,60 @@
 // Created by paracelsus on 2/13/2022.
 //
 
-#include "DescriptorMemoryManager.h"
+#include "DescriptorManager.h"
 #include <stdexcept>
 #include <iostream>
 
-DescriptorMemoryManager::DescriptorMemoryManager() {
+DescriptorManager::DescriptorManager() {
     blocksAvailable = 0;
     descriptorCount = 0;
     descriptorsPerBlock = 0;
 }
 
-DescriptorMemoryManager::DescriptorMemoryManager(const PersistentStorage &persistentStorage, unsigned int blocksAvailable) {
+DescriptorManager::DescriptorManager(const PersistentStorage &persistentStorage, unsigned int blocksAvailable) {
     this->persistentStorage = persistentStorage;
     this->blocksAvailable = blocksAvailable;
-    descriptorsPerBlock = persistentStorage.getBytesPerBlock() / sizeof(FileDescriptor);
+    descriptorsPerBlock = persistentStorage.blockSize() / sizeof(FileDescriptor);
     descriptorCount = 0;
-
 }
 
-void DescriptorMemoryManager::addDescriptor(FileDescriptor fileDescriptor) {
+unsigned DescriptorManager::addDescriptor(FileDescriptor fileDescriptor) {
     if (descriptorCount == blocksAvailable * descriptorsPerBlock) {
         throw std::runtime_error("Out of descriptor dedicated memory.");
     }
     auto block = descriptorCount / descriptorsPerBlock;
-    char content[persistentStorage.getBytesPerBlock()];
+    char content[persistentStorage.blockSize()];
     persistentStorage.read(block, content);
     auto positionInBlock = descriptorCount % descriptorsPerBlock;
     memcpy(content + positionInBlock * sizeof(FileDescriptor), &fileDescriptor, sizeof(FileDescriptor));
     persistentStorage.write(block, content);
-    ++descriptorCount;
+    return descriptorCount++;
 }
 
-FileDescriptor DescriptorMemoryManager::getDescriptor(unsigned int index) {
+FileDescriptor DescriptorManager::getDescriptor(unsigned int index) {
     if (index >= descriptorCount) {
         throw std::runtime_error("Index out of descriptor bounds.");
     }
     auto block = index / descriptorsPerBlock;
-    char content[persistentStorage.getBytesPerBlock()];
+    char content[persistentStorage.blockSize()];
     persistentStorage.read(block, content);
     auto positionInBlock = index % descriptorsPerBlock;
     return *reinterpret_cast<FileDescriptor *>(content + positionInBlock * sizeof(FileDescriptor));
 }
 
-void DescriptorMemoryManager::printAllDescriptors() {
+void DescriptorManager::updateDescriptor(unsigned int index, FileDescriptor fileDescriptor) {
+    if (index >= descriptorCount) {
+        throw std::runtime_error("Index out of descriptor bounds.");
+    }
+    auto block = index / descriptorsPerBlock;
+    char content[persistentStorage.blockSize()];
+    persistentStorage.read(block, content);
+    auto positionInBlock = index % descriptorsPerBlock;
+    memcpy(content + positionInBlock * sizeof(FileDescriptor), &fileDescriptor, sizeof(FileDescriptor));
+    persistentStorage.write(block, content);
+}
+
+void DescriptorManager::printAllDescriptors() {
     for (int i = 0; i < descriptorCount; ++i) {
         std::cout<<i<<": starting block = "<<getDescriptor(i).getStartingBlock()<<std::endl;
     }
---
+++ DescriptorManager.h
@@ -0,0 +1,27 @@
+//
+// Created by paracelsus on 2/13/2022.
+//
+
+#ifndef FILESYSTEM_SIMULATOR_DESCRIPTORMANAGER_H
+#define FILESYSTEM_SIMULATOR_DESCRIPTORMANAGER_H
+
+#include "PersistentStorage.h"
+#include "FileDescriptor.h"
+
+class DescriptorManager {
+private:
+    PersistentStorage persistentStorage;
+    unsigned descriptorCount;
+    unsigned blocksAvailable;
+    unsigned descriptorsPerBlock;
+public:
+    DescriptorManager(const PersistentStorage &persistentStorage, unsigned blocksAvailable);
+    DescriptorManager();
+    unsigned addDescriptor(FileDescriptor fileDescriptor);
+    FileDescriptor getDescriptor(unsigned index);
+    void updateDescriptor(unsigned index, FileDescriptor fileDescriptor);
+    void printAllDescriptors();
+};
+
+
+#endif //FILESYSTEM_SIMULATOR_DESCRIPTORMANAGER_H
