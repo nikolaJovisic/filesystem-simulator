894da1bbb840ac17dcaaae82cadd1f6ac824773a
ra74-2018 <nikolajovisic4@gmail.com>
2022-03-18 17:12:46 +0100

    feat: initialize ScatteredFilesystem

---
+++ scattered-filesystem/ScatteredFilesystem.cpp
@@ -0,0 +1,45 @@
+a//
+// Created by paracelsus on 3/18/2022.
+//
+
+#include "ScatteredFilesystem.h"
+
+int ScatteredFilesystem::open(std::string path) {
+    return 0;
+}
+
+void ScatteredFilesystem::close(unsigned int index) {
+
+}
+
+void ScatteredFilesystem::create(std::string path, bool isDirectory, unsigned int blocksReserved) {
+
+}
+
+void ScatteredFilesystem::read(unsigned int index, char *dst, unsigned int size) {
+
+}
+
+void ScatteredFilesystem::write(unsigned int index, char *src, unsigned int size) {
+
+}
+
+void ScatteredFilesystem::seek(unsigned int index, unsigned int position) {
+
+}
+
+void ScatteredFilesystem::remove(std::string path) {
+
+}
+
+void ScatteredFilesystem::serialize(std::string path) {
+
+}
+
+void ScatteredFilesystem::listContentsAt(std::string path) {
+
+}
+
+void ScatteredFilesystem::printState() {
+    Filesystem::printState();
+}
---
+++ scattered-filesystem/ScatteredFilesystem.h
@@ -0,0 +1,35 @@
+//
+// Created by paracelsus on 3/18/2022.
+//
+
+#ifndef FILESYSTEM_SIMULATOR_SCATTEREDFILESYSTEM_H
+#define FILESYSTEM_SIMULATOR_SCATTEREDFILESYSTEM_H
+
+
+#include "../filesystem/Filesystem.h"
+
+class ScatteredFilesystem : public Filesystem {
+public:
+    int open(std::string path) override;
+
+    void close(unsigned int index) override;
+
+    void create(std::string path, bool isDirectory, unsigned int blocksReserved) override;
+
+    void read(unsigned int index, char *dst, unsigned int size) override;
+
+    void write(unsigned int index, char *src, unsigned int size) override;
+
+    void seek(unsigned int index, unsigned int position) override;
+
+    void remove(std::string path) override;
+
+    void serialize(std::string path) override;
+
+    void listContentsAt(std::string path) override;
+
+    void printState() override;
+};
+
+
+#endif //FILESYSTEM_SIMULATOR_SCATTEREDFILESYSTEM_H
