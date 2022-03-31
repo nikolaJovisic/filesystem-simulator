8fe18a513a4e592fb740a146eb4ffdc22cac0b90
ra74-2018 <nikolajovisic4@gmail.com>
2022-03-31 14:21:28 +0200

    feat: add slice for reading

---
+++ scattered-filesystem/Slice.h
@@ -0,0 +1,16 @@
+//
+// Created by paracelsus on 3/31/2022.
+//
+
+#ifndef FILESYSTEM_SIMULATOR_SLICE_H
+#define FILESYSTEM_SIMULATOR_SLICE_H
+
+
+struct Slice {
+    unsigned block;
+    unsigned start;
+    unsigned length;
+};
+
+
+#endif //FILESYSTEM_SIMULATOR_SLICE_H
